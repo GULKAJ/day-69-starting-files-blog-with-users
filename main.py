78fec1e1bc29a3dbbc148535456b2dfc4bae9b0f
gulshan <gulshandhiman5600@gmail.com>
2025-09-25 19:13:34 +0100

    update DB_URI line

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db").replace("postgresql://", "postgresql+psycopg://")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
